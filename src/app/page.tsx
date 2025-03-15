6efb197ea507e11eaab2f409a0eaaf13813d9e85
PiscesXD <88549470+PiscesXD@users.noreply.github.com>
2025-03-16 03:18:22 +0800

    fix: table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
 'use client';
-import { useEffect, useState } from 'react';
-
-import { EarthquakeInfo } from '@/modal/earthquake-info';
 import EarthquakeInfoTable from '@/components/earthquake-info-table';
 
 export default function HomePage() {
-  const [earthquakeInfo, setEarthquakeInfo] = useState<Array<EarthquakeInfo>>([]);
-
-  useEffect(() => {
-    if (!earthquakeInfo.length) return;
-    console.log(earthquakeInfo);
-  }, [earthquakeInfo]);
-
-  useEffect(() => {
-    const fetchEarthquakeInfo = async () => {
-      const res = await fetch(`https://api-1.exptech.dev/api/v2/eq/report?limit=50&key=`);
-      const ans = await res.json() as Array<EarthquakeInfo>;
-      setEarthquakeInfo(ans);
-    };
-    void fetchEarthquakeInfo();
-  }, []);
-
   return (
     <EarthquakeInfoTable />
   );
--- src/components/earthquake-info-table.tsx
+++ src/components/earthquake-info-table.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+
+import { EarthquakeInfo } from '@/modal/earthquake-info';
+
 import {
   Table,
   TableBody,
@@ -55,30 +59,60 @@ const invoices = [
 ];
 
 export default function EarthquakeInfoTable() {
-  return (
+  const [earthquakeInfo, setEarthquakeInfo] = useState<Array<EarthquakeInfo>>([]);
 
+  useEffect(() => {
+    if (!earthquakeInfo.length) return;
+    console.log(earthquakeInfo);
+  }, [earthquakeInfo]);
+
+  useEffect(() => {
+    const fetchEarthquakeInfo = async () => {
+      const res = await fetch(`https://api-1.exptech.dev/api/v2/eq/report?limit=50&key=`);
+      const ans = await res.json() as Array<EarthquakeInfo>;
+      setEarthquakeInfo(ans);
+    };
+    void fetchEarthquakeInfo();
+  }, []);
+
+  return (
     <div>
       <div className="p-4 text-center text-3xl font-bold">
         TREM EEW
       </div>
       <div className="pr-4 pl-4">
-        <Table>
-          <TableCaption>Taiwan Real-time Earthquake Monitoring｜2025｜ExpTech Studio</TableCaption>
+        <Table className="w-full border-collapse border border-gray-300">
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="w-[100px]">No.</TableHead>
+              <TableHead>EventID</TableHead>
+              <TableHead>發表單位</TableHead>
+              <TableHead className="text-center">報數</TableHead>
+              <TableHead className="text-center">檢知時刻</TableHead>
+              <TableHead className="text-center">發震時刻(CWA)</TableHead>
+              <TableHead className="text-center">震央地名</TableHead>
+              <TableHead className="text-center">北緯</TableHead>
+              <TableHead className="text-center">東經</TableHead>
+              <TableHead className="text-center">深度</TableHead>
+              <TableHead className="text-center">規模</TableHead>
+              <TableHead className="text-center">預估最大震度</TableHead>
+              <TableHead className="text-center">實測最大震度(CWA)</TableHead>
+              <TableHead className="text-center">
+                實測最大長周期
+                <br />
+                地震動階級
+              </TableHead>
+              <TableHead className="text-center">RTS</TableHead>
+
             </TableRow>
           </TableHeader>
           <TableBody>
             {invoices.map((invoice) => (
               <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                <TableCell className="border border-gray-300">{invoice.invoice}</TableCell>
+                <TableCell className="border border-gray-300">{invoice.paymentStatus}</TableCell>
+                <TableCell className="border border-gray-300">{invoice.paymentMethod}</TableCell>
+                <TableCell className="border border-gray-300 text-right">{invoice.totalAmount}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -88,6 +122,7 @@ export default function EarthquakeInfoTable() {
               <TableCell className="text-right">$2,500.00</TableCell>
             </TableRow>
           </TableFooter>
+          <TableCaption>Taiwan Real-time Earthquake Monitoring｜2025｜ExpTech Studio</TableCaption>
         </Table>
       </div>
     </div>
